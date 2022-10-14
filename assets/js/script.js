9c4e3f5c7ef8162364806f26dadf00cea585b30a
Hushmist <nurtai.0624@gmail.com>
2022-10-14 19:48:55 +0600

    Create script.js

---
+++ assets/js/script.js
@@ -0,0 +1,47 @@
+$(document).ready(function () {
+     $('.form-check-input').click(function () {
+        		console.log(123)
+        // if(document.document.getElementsByClassName("form-check-input").checked) {
+        // 		console.log(1)
+        // 		document.getElementById('text').style = "text-decoration:line-through";
+        // 	} else {
+        // 		console.log(0)
+        // 		document.getElementById('text').style = "";
+        // 	}
+    });
+})
+
+
+
+
+
+// let input = document.getElementById('text').style = "text-decoration:line-through";
+
+// input.setAttribute('checked', '');
+    // document.getElementById("myCheck").checked = true;
+
+// function check() {
+// 	// console.log(123)
+// 	if(document.document.getElementsByClassName("form-check-input").checked) {
+// 		console.log(1)
+// 		document.getElementById('text').style = "text-decoration:line-through";
+// 	} else {
+// 		console.log(0)
+// 		document.getElementById('text').style = "";
+// 	}
+
+	// if (document.getElementById("form-check-input").checked === true) {
+ //    	document.getElementById('text').style = "text-decoration:line-through";
+
+	// };
+// }
+
+// function uncheck() {
+//     document.getElementById("myCheck").checked = false;
+// }
+
+
+// if( $('.form-check-input').getAttribute('checked')) {
+// 	console.log(123)
+// }
+console.log(123)
